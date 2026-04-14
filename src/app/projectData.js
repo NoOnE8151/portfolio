8e1c0242532ac3781bfb230d50c563576941c850
Lomash Jangde <152585076+NoOnE8151@users.noreply.github.com>
2026-04-14 15:45:06 +0530

    Modify projectData.js with new project details
    Updated project data for List IQ and Digimart. Changed IDs and descriptions for clarity.

--- src/app/projectData.js
+++ src/app/projectData.js
@@ -1,8 +1,23 @@
 export const projectsData = [
-  {
+    {
     id: 1,
+    title: "List IQ",
+    category: "AI-powered eCommerce SaaS",
+    image: "/assets/projects/listIq.png",
+    description: "List IQ is an AI-powered eCommerce listing generator SaaS that helps sellers create high-converting product titles, descriptions, and bullet points. It leverages AI to generate SEO-optimized and conversion-focused content for platforms like Amazon, Flipkart, and Myntra —reducing manual effort and improving listing performance.",
+      techStack: [
+      "Next.js",
+      "Tailwind CSS",
+      "MongoDB/Mongoose",
+      "Clerk Authentication",
+      "Razorpay SDK"
+    ],
+    link: "https://listiq-pi.vercel.app/",
+  },
+  {
+    id: 2,
     title: "Digimart",
-    category: "E Commerce",
+    category: "E Commerce Platform",
     image: "/assets/projects/digimart.png",
     description:
       "DigiMart is a fully functional e-commerce application built from the ground up using the MERN stack. The platform delivers a complete online shopping experience with secure user authentication, product management, cart and order handling, and an intuitive, responsive interface optimised for both mobile and desktop users. The system includes an admin dashboard for managing inventory, product listings, and user data, along with seamless checkout and payment integration. The project demonstrates strong capability in full-stack architecture, RESTful API design, state management, and clean UI development. It reflects practical understanding of building production-ready applications end-to-end.",
@@ -14,7 +29,7 @@ export const projectsData = [
     ],
     link: "https://digimart-lime.vercel.app/",
   }, {
-    id: 2,
+    id: 3,
     title: "Github Insight",
     category: "API Integration Project",
     description: "A React-based tool that lets users search any GitHub profile and instantly view key details such as avatar, bio, repositories, followers, and activity. It showcases clean API integration, dynamic data rendering, and a responsive interface.",
